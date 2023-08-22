0eab9e2bc2bd61d061beba65dcd71cb5b8084980
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-08-22 22:17:47 +0530

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -5,38 +5,44 @@ import Gallery from './components/Gallery/Gallery';
 
 function App() {
 
-  const [data, setData] = useState();
+  let photos;
+  const [data, setData] = useState([]);
   const [offset, setOffset] = useState(0);
-  const [limit, setLimit] = useState(1);
+  const [limit, setLimit] = useState(0);
+
+  
+  useEffect(() =>{
+    handleImageFetching();
+  }, []);
+  
+  async function handleImageFetching(){
+    try{
+      const Info = await AxiosInstance.get('/', {
+        params : {
+          offset : {offset},
+          limit : 20
+        }
+      });
+      
+      photos = Info.data.photos;
+      setData([...data, photos[0]])
+      
+    }catch(err){
+      return console.log(err.message);
+    }
+  } 
 
   useEffect(() =>{
       setTimeout(()=>{
           if(limit < 20){
-            setLimit((l) => l + 1);
+            photos &&   
+              setLimit((l) => l + 1);
+              console.log(photos[limit]);
+              setData([...data, {...photos[limit]}]);
+            
           }
       }, 500)
   }, [limit])
-
-  useEffect(() =>{
-    handleImageFetching();
-  }, [limit]);
-
-  async function handleImageFetching(){
-    try{
-        const Info = await AxiosInstance.get('/', {
-          params : {
-            offset : {offset},
-            limit
-          }
-        });
-
-        const { photos } = Info.data;
-        setData(photos);
-
-    }catch(err){
-        return console.log(err.message);
-    }
-  } 
 
   return (
     <>
